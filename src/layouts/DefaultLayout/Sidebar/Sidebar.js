21e23cf7d96d4cbb96f6542090f13ae69c349eff
minhthanh195 <blueskyalone199@gmail.com>
2024-05-27 16:44:50 +0700

    Xây dựng UI phần sidebar #1

--- src/layouts/DefaultLayout/Sidebar/Sidebar.js
+++ src/layouts/DefaultLayout/Sidebar/Sidebar.js
@@ -1,12 +1,37 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
-
+import config from '~/config';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    UserGroupIcon,
+    LiveIcon,
+    HomeIconActive,
+    UserGroupIconActive,
+    LiveIconActive,
+} from '~/components/Icons';
 const cx = classNames.bind(styles);
 
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
-            <h2>Sidebar</h2>
+            <Menu>
+                <MenuItem
+                    title="For Your"
+                    to={config.routes.home}
+                    icon={<HomeIcon />}
+                    iconActive={<HomeIconActive />}
+                />
+
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    iconActive={<UserGroupIconActive />}
+                />
+
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} iconActive={<LiveIconActive />} />
+            </Menu>
         </aside>
     );
 }
