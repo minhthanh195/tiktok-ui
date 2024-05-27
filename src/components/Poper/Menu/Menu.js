6cc09024718ea9a008478384682437ccf936cea5
minhthanh195 <blueskyalone199@gmail.com>
2024-05-27 14:07:38 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/components/Poper/Menu/Menu.js
@@ -0,0 +1,74 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/components/Poper';
+
+import styles from './Menu.module.scss';
+import MenuItem from './MenuItem';
+import Header from './Header';
+import { useState } from 'react';
+
+const cx = classNames.bind(styles);
+
+const defaultFn = () => {};
+
+function Menu({ children, hideOnClick = false, items = [], onChange = defaultFn }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+
+    const renderItems = () => {
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
+        });
+    };
+
+    return (
+        <Tippy
+            interactive
+            delay={[0, 300]}
+            offset={[12, 8]}
+            placement="bottom-end"
+            hideOnClick={hideOnClick}
+            render={(attrs) => (
+                <div className={cx('menu_list')} tabIndex={-1} {...attrs}>
+                    <PopperWrapper className={cx('menu_poper')}>
+                        {history.length > 1 && (
+                            <Header
+                                title={current.title}
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1));
+                                }}
+                            ></Header>
+                        )}
+                        <div className={cx('menu_body')}>{renderItems()}</div>
+                    </PopperWrapper>
+                </div>
+            )}
+            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    hideOnClick: PropTypes.bool,
+    items: PropTypes.array,
+    onChange: PropTypes.func,
+};
+export default Menu;
